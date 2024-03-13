afdae496cf22eb23510ddcda885530a3ee4c7519
Vijaysingh147 <161139887+Vijaysingh147@users.noreply.github.com>
2024-03-13 10:58:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6133434192:AAHDkrhW9sgqzLU3ay4o7GRy_yPyDuWJdIc",
-             api_id= 27495136,
-             api_hash= "4ccc4865eec4d8fde7530e71948b3424")
+             bot_token= "6417967535:AAGuFnZqK5YSnI5joKvg4a1a9h-Xqhl_jdU",
+             api_id= 23004101,
+             api_hash= "a2e157e87728053027cbb156e41a832a")
 
 
 @bot.on_message(filters.command(["start"]))
